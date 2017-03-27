b6456aa1225690b41f9cbaabad16d480d0f91255
Bartosz Kościów <bkosciow@davinsi-studio.eu>
2017-03-27 10:32:53 +0200

    symfony console detection

--- wconsole.ps1
+++ wconsole.ps1
@@ -1,2 +1,10 @@
-﻿Write-Host "docker-compose exec php bin-symfony/console $args"
-docker-compose exec php bin-symfony/console $args
+﻿if (Test-Path ./bin-symfony) {
+    Write-Host "docker-compose exec php bin-symfony/console $args"
+    docker-compose exec php bin-symfony/console $args
+} ElseIf (Test-Path ./app/console) {
+    Write-Host "docker-compose exec php app/console $args"
+    docker-compose exec php app/console $args
+} Else {
+    Write-Host "docker-compose exec php bin/console $args"
+    docker-compose exec php bin/console $args
+}
